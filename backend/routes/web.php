221b6abd152f88468152f1a6a761152464562c0e
kuroshun960 <61528468+kuroshun960@users.noreply.github.com>
2021-01-24 19:02:18 +0900

    マイグレーション ファイル

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -33,3 +33,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
