4662f0e43c8402795be29977b9340d7c5cd2233b
kurosaki_shunsuke <kurosaki_shunsuke@oisixradaichi.co.jp>
2021-05-26 15:20:44 +0900

    Update navbar.blade.php

--- backend/resources/views/commons/navbar.blade.php
+++ backend/resources/views/commons/navbar.blade.php
@@ -2,6 +2,7 @@
 
 
         {{-- ナビゲーションバー --}}
+        
             
         
         <nav class="">
@@ -54,7 +55,7 @@
                     <div class="registAndlogin__btn">
                         {{-- ユーザ登録ページへのリンク --}}
                         {{--     {!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav_item nav__btn nav__register__btn']) !!} --}}
-                        
+
                         {{--ログインへのリンク --}}
                             {!! link_to_route('login', 'ログイン', [], ['class' => 'nav_item nav__btn']) !!}
 
